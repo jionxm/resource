d9ebf62527bb135a943902fdc041c4be33d55f0a
陈俊 <492536848@qq.com>
2018-10-22 13:50:55 +0800

    fix#通过定时任务管理运行文件扫描脚本并将扫描到的数据插入到数据库

---
+++ src/main/java/com/tedu/base/scanFileFirectory/service/ScanFileFirectoryService.java
@@ -0,0 +1,21 @@
+package com.tedu.base.scanFileFirectory.service;
+
+
+/**
+ * 文件目录获取
+ * @author Chenjun
+ *
+ */
+public interface ScanFileFirectoryService {
+
+	/**
+	 * 
+	 * @Description: 获取文件目录并插入到数据库文件表
+	 * @author: Chenjun
+	 * @date: 2018年10月22日 上午12:06:33  
+	 * @param:      
+	 * @return: void
+	 */
+	public void gainScanFileFirectory();
+
+}
---
+++ src/main/resources/engine/info/sql/resource/InsertResource.sql.ftl
@@ -0,0 +1,4 @@
+insert into t_file_data
+(name,path) 
+values(
+#{data.srcName},#{data.srcPath})
